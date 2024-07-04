5b7d029821b3e5b87f44e021eec74de685f86ae5
Nfislam <nfislam11@gmail.com>
2024-07-04 15:41:40 -0400

    5th commit - added progressbar and image button in home

--- NaimulModule7/src/main/java/naimul/islam/n01586149lab7/SecondFragment.java
+++ NaimulModule7/src/main/java/naimul/islam/n01586149lab7/SecondFragment.java
@@ -7,6 +7,9 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.ProgressBar;
+import android.widget.Toast;
 
 
 import androidx.fragment.app.Fragment;
@@ -14,16 +17,80 @@ import java.io.*;
 
 public class SecondFragment extends Fragment {
 
-    public SecondFragment(){
-        // require a empty public constructor
+        // TODO: Rename parameter arguments, choose names that match
+        // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+        private static final String ARG_PARAM1 = "param1";
+        private static final String ARG_PARAM2 = "param2";
+
+        // TODO: Rename and change types of parameters
+        private String mParam1;
+        private String mParam2;
+
+        private int counter = 0;
+
+        public SecondFragment() {
+            // Required empty public constructor
+        }
+
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment SecondFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        public static SecondFragment newInstance(String param1, String param2) {
+            SecondFragment fragment = new SecondFragment();
+            Bundle args = new Bundle();
+            args.putString(ARG_PARAM1, param1);
+            args.putString(ARG_PARAM2, param2);
+            fragment.setArguments(args);
+            return fragment;
+        }
+
+        @Override
+        public void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            if (getArguments() != null) {
+                mParam1 = getArguments().getString(ARG_PARAM1);
+                mParam2 = getArguments().getString(ARG_PARAM2);
+            }
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                                 Bundle savedInstanceState) {
+            // Inflate the layout for this fragment
+            View view = inflater.inflate(R.layout.fragment_second, container, false);
+
+            ProgressBar progressBar= view.findViewById(R.id.NaiProgressBar); // initiate the progress bar
+            progressBar.setProgress(0);
+
+
+            ImageButton imageButton = view.findViewById(R.id.NaiImageButton);
+            imageButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    counter++;
+                    progressBar.setProgress(counter * 20);
+                    Toast.makeText(getActivity(), getString(R.string.naimul) + counter, Toast.LENGTH_SHORT).show();
+
+                    if (counter == 5) {
+                        progressBar.setVisibility(View.GONE);
+                    }
+
+                    if (counter == 8) {
+                        System.exit(0); // Exit the application
+                    }
+                }
+            });
+
+            return view;
+        }
     }
 
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-
-        return inflater.inflate(R.layout.fragment_second, container, false);
-    }
-}
 
 
 ///**
