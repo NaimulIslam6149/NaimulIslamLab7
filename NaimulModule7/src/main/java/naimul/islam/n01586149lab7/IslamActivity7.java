52874d826420ddc00e19bbd4c447af802399ebdf
Nfislam <nfislam11@gmail.com>
2024-07-04 15:30:40 -0400

    4th commit - added top menu button

--- NaimulModule7/src/main/java/naimul/islam/n01586149lab7/IslamActivity7.java
+++ NaimulModule7/src/main/java/naimul/islam/n01586149lab7/IslamActivity7.java
@@ -1,15 +1,19 @@
 package naimul.islam.n01586149lab7;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 
 import android.os.Bundle;
+import android.provider.Settings;
+import android.view.Menu;
 import android.view.MenuItem;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,50 +21,55 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.io.*;
 
-public class IslamActivity7 extends AppCompatActivity
-        implements BottomNavigationView.OnNavigationItemSelectedListener {
+public class IslamActivity7 extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
 
     BottomNavigationView bottomNavigationView;
+    Toolbar toolbar;
+    FirstFragment firstFragment = new FirstFragment();
+    SecondFragment secondFragment = new SecondFragment();
+    ThirdFragment thirdFragment = new ThirdFragment();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Setup BottomNavigationView
         bottomNavigationView = findViewById(R.id.bottomNavigationView);
         bottomNavigationView.setOnItemSelectedListener(this);
         bottomNavigationView.setSelectedItemId(R.id.person);
+
+        toolbar = findViewById(R.id.NaiToolbar);
+
     }
 
-    FirstFragment firstFragment = new FirstFragment();
-    SecondFragment secondFragment = new SecondFragment();
-    ThirdFragment thirdFragment = new ThirdFragment();
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_settings) {
+            //displayTextView.setText("You selected start!");
+            Intent intent = new Intent(Settings.ACTION_SETTINGS);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 
     @Override
     public boolean onNavigationItemSelected(@NonNull MenuItem item) {
         int itemId = item.getItemId();
 
         if (itemId == R.id.person) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, firstFragment)
-                    .commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, firstFragment).commit();
             return true;
         }
 
         if (itemId == R.id.home) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, secondFragment)
-                    .commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, secondFragment).commit();
             return true;
         }
 
         if (itemId == R.id.settings) {
-            getSupportFragmentManager()
-                    .beginTransaction()
-                    .replace(R.id.flFragment, thirdFragment)
-                    .commit();
+            getSupportFragmentManager().beginTransaction().replace(R.id.flFragment, thirdFragment).commit();
             return true;
         }
 
